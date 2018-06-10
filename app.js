9e9a3f7d552a6f8d952da184b84e6f155b76fe80
Przemyslaw Sulecki <przsul000@utp.edu.pl>
2018-06-10 11:00:26 +0200

    Added reading from database

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'))
+
+var mysql      = require('mysql');  
+var connection = mysql.createConnection({  
+  host     : 'localhost',  
+  user     : 'przemek',  
+  password : '',  
+  database : 'zamekutp'  
+});        
+
+app.get('/rows', function (req, res) {
+
+  connection.query('SELECT * FROM persons', function(err, rows, fields)   
+  {  
+
+      if (err) throw err;  
+
+      res.json(rows); 
+
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
